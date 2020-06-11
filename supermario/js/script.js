7d4d6489e7d8444f350c1007503dbe7bd74b9014
Mario Perez Olea <mjapo@live.co.uk>
2020-06-11 22:47:02 +0000

    Fixed some issues on js and css codes

--- supermario/js/script.js
+++ supermario/js/script.js
@@ -1,101 +1,85 @@
-const cardsArray = [
-    {
-        name: "shell",
-        img: "img/blueshell.png",
-    },
-    {
-        name: "star",
-        img: "img/star.png",
-    },
-    {
-        name: "bobomb",
-        img: "img/bobomb.png",
-    },
-    {
-        name: "mario",
-        img: "img/mario.png",
-    },
-    {
-        name: "luigi",
-        img: "img/luigi.png",
-    },
-    {
-        name: "peach",
-        img: "img/peach.png",
-    },
-    {
-        name: "1up",
-        img: "img/1up.png",
-    },
-    {
-        name: "mushroom",
-        img: "img/mushroom.png",
-    },
-    {
-        name: "thwomp",
-        img: "img/thwomp.png",
-    },
-    {
-        name: "bulletbill",
-        img: "img/bulletbill.png",
-    },
-    {
-        name: "coin",
-        img: "img/coin.png",
-    },
-    {
-        name: "goomba",
-        img: "img/goomba.png",
-    },
-    {
-        name: "toadette",
-        img: "img/toadette.png",
-    },
-    {
-        name: "wario",
-        img: "img/wario.png",
-    },
-    {
-        name: "yoshi",
-        img: "img/yoshi.png",
-    },
-    {
-        name: "donkey-kong",
-        img: "img/donkey-kong.jpg",
-    },
-    {
-        name: "daisy",
-        img: "img/daisy.jpg",
-    },
-    {
-        name: "bowser",
-        img: "img/bowser.png",
-    },
-]
+var cardsArray = [{
+        "name": "shell",
+        "img": "img/blueshell.png"
+    }, {
+        "name": "star",
+        "img": "img/star.png"
+    }, {
+        "name": "bobomb",
+        "img": "img/bobomb.png"
+    }, {
+        "name": "mario",
+        "img": "img/mario.png"
+    }, {
+        "name": "luigi",
+        "img": "img/luigi.png"
+    }, {
+        "name": "peach",
+        "img": "img/peach.png"
+    }, {
+        "name": "1up",
+        "img": "img/1up.png"
+    }, {
+        "name": "mushroom",
+        "img": "img/mushroom.png"
+    }, {
+        "name": "thwomp",
+        "img": "img/thwomp.png"
+    }, {
+        "name": "bulletbill",
+        "img": "img/bulletbill.png"
+    }, {
+        "name": "coin",
+        "img": "img/coin.png"
+    }, {
+        "name": "goomba",
+        "img": "img/goomba.png"
+    }, {
+        "name": "toadette",
+        "img": "img/toadette.png"
+    }, {
+        "name": "wario",
+        "img": "img/wario.png"
+    }, {
+        "name": "yoshi",
+        "img": "img/yoshi.png"
+    }, {
+        "name": "donkey-kong",
+        "img": "img/donkey-kong.jpg"
+    }, {
+        "name": "daisy",
+        "img": "img/daisy.jpg"
+    }, {
+        "name": "bowser",
+        "img": "img/bowser.png"
+    }];
+var gameGrid = cardsArray.concat(cardsArray).sort(function () {
+    return 0.5 - Math.random();
+});
 
-let gameGrid = cardsArray.concat(cardsArray);
-gameGrid.sort(() => 0.5 - Math.random());
+var firstGuess = "";
+var secondGuess = "";
+var count = 0;
+var previousTarget = null;
+var delay = 1200;
 
-let firstGuess = "";
-let secondGuess = "";
-let count = 0;
-let previousTarget = null;
-let delay = 1200;
-
-const game = document.getElementById("game");
-const grid = document.createElement("section");
+var game = document.getElementById("game");
+var grid = document.createElement("section");
 grid.setAttribute("class", "grid");
 game.appendChild(grid);
 
-gameGrid.forEach(item => {
-    const { name, img } = item;
-    const card = document.createElement("div");
+gameGrid.forEach(function (item) {
+    var name = item.name,
+        img = item.img;
+    
+    var card = document.createElement("div");
     card.classList.add("card");
     card.dataset.name = name;
 
-    const front = document.createElement("div");
+    var front = document.createElement("div");
     front.classList.add("front");
-    const back = document.createElement("div")
+    
+    var back = document.createElement("div");
     back.classList.add("back");
     back.style.backgroundImage = `url(${img})`;
     
@@ -104,34 +88,29 @@ gameGrid.forEach(item => {
     card.appendChild(back);
 });
 
-const match = () => {
-    const selected = document.querySelectorAll(".selected");
-    selected.forEach(card => {
+var match = function match() {
+    var selected = document.querySelectorAll(".selected");
+    selected.forEach(function (card) {
         card.classList.add("match");
     });
 };
 
-const resetGuesses = () => {
+var resetGuesses =  function resetGuesses() {
     firstGuess = "";
     secondGuess ="";
     count = 0;
     previousTarget = null;
 
     var selected = document.querySelectorAll(".selected");
-    selected.forEach(card => {
+    selected.forEach(function (card) {
         card.classList.remove("selected");
     });
 };
 
 
-grid.addEventListener("click", function(event) {
-    let clicked = event.target
-    if (
-        clicked.nodeName === "SECTION" ||
-        clicked === previousTarget ||
-        clicked.parentNode.classList.contains("selected") ||
-        clicked.parentNode.classList.contains("match")
-    ) {
+grid.addEventListener("click", function (event) {
+    var clicked = event.target;
+    if (clicked.nodeName === "SECTION" || clicked === previousTarget || clicked.parentNode.classList.contains("selected") || clicked.parentNode.classList.contains("match")) {
         return;
     }
 
@@ -141,18 +120,18 @@ grid.addEventListener("click", function(event) {
             firstGuess = clicked.parentNode.dataset.name;
             console.log(firstGuess);
             clicked.parentNode.classList.add("selected");
-            } else {
-                secondGuess = clicked.parentNode.dataset.name;
-                console.log(secondGuess);
-                clicked.parentNode.classList.add("selected");
+        } else {
+            secondGuess = clicked.parentNode.dataset.name;
+            console.log(secondGuess);
+            clicked.parentNode.classList.add("selected");
+        }
+        if (firstGuess && secondGuess) {
+            if (firstGuess === secondGuess) {
+                setTimeout(match, delay);
             }
-
-            if (firstGuess && secondGuess) {
-                if (firstGuess === secondGuess) {
-                    setTimeout(match, delay);
-                }
-                previousTarget = clicked;
-            }
+            setTimeout(resetGuesses, delay);
+        }
+        previousTarget = clicked;
     }
 });
 
